b54a9718ee61b655a19cbb8b50f28bc556071886
Eugene Joseph Koontz <ekoontz@apache.org>
2012-12-10 22:14:54 -0800

    starting up all services: nn,jn,zk,zkfc on host; nn,zkfc on guest

--- terms.rb
+++ terms.rb
@@ -16,26 +16,34 @@ end
 conf = YAML.load_file ARGV.shift
 
 nn1 = conf['nn1']
+nn2 = conf['nn2']
 nn1log = conf['nn1log']
 dn1 = conf['dn1']
 nn1log = conf['dn1log']
 zk = conf['zk']
 
 write_term_script "terms.scpt", [
-                                 [conf['nn1'], conf['nn1log'],   "{250,25,  1500,180}", "nn"],
+                                 [conf['nn1'], conf['jnlog'],    "{250,1825,1500,540}", "jn",
+                                  "cd hadoop-conf && make format-and-start-jn"],
 
-                                 [conf['dn1'], conf['dn1log'],   "{250,475, 1500,300}", "dn"],
-
-                                 [conf['zk'],  nil,              "{1550,475,1800,420}", "zk", 
+                                 [conf['zk'],  nil,              "{1350,475,1800,420}", "zk",
                                   "cd hadoop-conf && make start-zk"],
 
-                                 [conf['nn1'], conf['zkfc1log'], "{ 250,925,1500,420}", "zkfc1"],
+                                 [conf['nn1'], conf['zkfc1log'], "{ 250,925,1500,420}", "zkfc1",
+                                 "cd hadoop-conf && make start-zkfc"],
 
-                                 [conf['nn1'], conf['jnlog'],    "{250,1825,1500,540}", "jn"],
+                                 [conf['dn1'], conf['dn1log'],   "{250,475, 1500,300}", "dn",
+                                  "cd hadoop-conf && make format-and-start-dn"],
 
-                                 [conf['nn2'], conf['nn2log'],   "{250,2275,1500,660}", "nn2"],
+                                 [conf['nn1'], conf['nn1log'],   "{250,25,  1500,180}", "nn",
+                                  "cd hadoop-conf && make format-and-start-master"],
 
-                                 [conf['nn2'], conf['zkfc2log'], "{250,2725,1500,780}", "zkfc2"]
+                                 [conf['nn2'], conf['zkfc2log'], "{250,2725,1500,780}", "zkfc2",
+                                  "ssh #{nn2} '. .bash_profile && cd hadoop-conf && make stop && make start-zkfc'"],
+
+                                 [conf['nn2'], conf['nn2log'],   "{250,2275,1500,660}", "nn2",
+                                  "ssh #{nn2} '. .bash_profile && cd hadoop-conf && sleep 30 && make start-standby-nn-on-guest'"]
+
 
                                 ]
 
